b379cb9f09598c6d08ad4e8d286732c73cac6ac0
edmondchan027 <edmondchan027@gmail.com>
2024-06-25 18:57:00 +0930

    Modded side menu code

--- scripts/menu.js
+++ scripts/menu.js
@@ -4,53 +4,133 @@ var currentpage = parts.pop(parts);
 
 /* CHANGE HERE IF YOU WANT TO EDIT THE MENU */
 
+/* Main menu list*/
+// [Html file , Display text]
+// or, if there will be submenu
+// [Html file , Displaytext , [submenulist]]
+// Sub menu follows the excact format
+
 const menu = [
-    ["maimaiGuide_main.html", "Home"],
-    ["maimaiGuide_BeforeYouStart.html", "Before You Start"],
-    ["maimaiGuide_GameModes.html", "Game Modes"],
-    ["maimaiGuide_AboutAuthors.html", "About Authors"],
-    ["maimaiGuide_OfficialLinks.html", "Official Links"]
+
+
+    //Home tab
+    ["Home.html", "Home"],
+
+    //Before you play tab
+    ["BeforeYouPlay.html", "Before You Play"],
+
+    //Beginner tab
+    ["Beginner.html", "Beginners",
+        [
+            //Beginner Submenu tab
+            ["Beg-Options.html","Recommended options"],
+            ["Beg-Options.html","1"],
+            ["Beg-Options.html","2"],
+            ["Beg-Options.html","3"],
+            ["Beg-Options.html","4"],
+            ["Beg-Options.html","5"]
+        ]
+    ],
+
+    //Intermediate tab
+    ["Intermediate.html", "Intermediate",
+        [
+            //Intermediate Submenu tab
+            ["Int-Options.html","Recommended options"]
+        ]
+    ],
+
+    //Advanced tab
+    ["Advanced.html", "Beginners",
+        [
+            //Beginner Submenu tab
+            ["Adv-Options.html","Recommended options"]
+        ]
+    ],
+
+    //Etiquettes tab
+    ["Etiquettes.html","Arcade Etiquettes"],
+
+    //AboutAuthors tab
+    ["AboutAuthors.html","About Authors"],
+
+    //OfficialLinks tab
+    ["OfficialLinks.html","Official links"],
+
+    //Contacts tab
+    ["Contacts.html","Contacts"]
 ];
-const submenu = [
-    ["maimaiGuide_Beginner.html", "Beginner Tips"],
-    ["maimaiGuide_Intermediate.html", "Intermediate Tips"],
-    ["maimaiGuide_Advanced.html", "Advanced Tips"]
-];
+
+/* Rendering */
+
+renderMenu(menu.length,menu);
 
 /* helper functions */
 
-function expand(e)
+/* drop down menu button click */
+function DropdownClick(e,listnum)
 {
-    var dropdownContent = this.document.getElementById("how-to-play-menu");
-    console.log(dropdownContent);
-    if (dropdownContent.style.display === "block")
-        dropdownContent.style.display = "none";
-    else
-        dropdownContent.style.display = "block";
-}
-function renderMenu(start, end, menu)
-{
-    let content = "";
-    for (let i = start; i < end + 1; i ++)
-    {
-        if (menu[i][0] == currentpage)
-            content += '<li class="active"><a>'+menu[i][1]+'</a><li>';
-        else
-            content += '<li><a href="'+menu[i][0]+'">'+menu[i][1]+'</a></li>';
+
+    console.log(listnum);
+    var DropDownButton = this.document.getElementById("Button"+listnum);
+    var Submenu = this.document.getElementById("submenu"+listnum);
+    html="";
+
+    //Expand submenu
+    if(DropDownButton.innerHTML=='v'){
+        DropDownButton.innerHTML='^';
+        for(let i=0;i<menu[listnum][2].length;i++){
+            html += '<li><a href='+menu[listnum][2][i][0]+'>'+menu[listnum][2][i][1]+'</a></li>';
+        }
+        Submenu.innerHTML += html;
     }
-    return content;
+    //Contract menu
+    else{
+        DropDownButton.innerHTML='v';
+        Submenu.innerHTML = "";
+    }
+
+    html ="";
+
+
 }
 
-/* rendering */
+function renderMenu(menulength, menulist)
+{
+    let html = "";
 
-var html = "";
-html += renderMenu(0, 1, menu);
-/* Drop down submenu render */
-html += '<li><a style="width:70%" href="maimaiGuide_HowToPlay.html">How To Play</a><button class="dropdown-btn" onclick="expand(this)">v</button></li>';
-html += '<div id="how-to-play-menu" class="dropdown-div">';
-html += renderMenu(0, submenu.length - 1, submenu);
-html += '</div>'
-/* Continue rendering the main menu */
-html += renderMenu(2, menu.length - 1, menu);
+    //render every array list in menu
+    for (let i=0;i<menulength;i++){
 
-document.getElementById('main_menu').innerHTML = html;
+        console.log("menulength = " + menulength + " i = "+ i + " menulist[i].length = " + menulist[i].length);
+
+        //adding list item
+        html += '<li '
+
+        //checks if the page is a current page
+        if(menu[i][0] == currentpage){
+            //add active class
+            html += 'class="active">';
+        }
+        else{
+            //do not add active class
+            html += '>';
+        }
+
+        //adding href
+        html += '<a href='+menu[i][0]+'>'+menu[i][1]+'</a>';
+        
+        //add a button if theres a submenu
+        if(menu[i].length == 3){
+            html += '<button class="dropdown-btn" id=Button'+i+' onclick="DropdownClick(this,'+ i +')">v</button></a> <ul id="submenu'+i+'"></ul>';
+        }
+
+        html += '</li>';
+        
+        document.getElementById('main_menu').innerHTML += html;
+        html = ""; 
+    }
+
+}
+
+
