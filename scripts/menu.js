b7d27b71fe67a2ce6bab05ecbaf1f5e6278953a4
shadowshake <honghonglee2000@gmail.com>
2024-06-26 02:43:01 +0930

    modidied menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -11,27 +11,18 @@ var currentpage = parts.pop(parts);
 // Sub menu follows the excact format
 
 const menu = [
-
-
     //Home tab
     ["Home.html", "Home"],
-
     //Before you play tab
     ["BeforeYouPlay.html", "Before You Play"],
-
     //Beginner tab
     ["Beginner.html", "Beginners",
         [
             //Beginner Submenu tab
             ["Beg-Options.html","Recommended options"],
-            ["Beg-Options.html","1"],
-            ["Beg-Options.html","2"],
-            ["Beg-Options.html","3"],
-            ["Beg-Options.html","4"],
-            ["Beg-Options.html","5"]
+            ["Beg-Optons2.html","2"]
         ]
     ],
-
     //Intermediate tab
     ["Intermediate.html", "Intermediate",
         [
@@ -39,60 +30,35 @@ const menu = [
             ["Int-Options.html","Recommended options"]
         ]
     ],
-
     //Advanced tab
-    ["Advanced.html", "Beginners",
+    ["Advanced.html", "Advanced",
         [
             //Beginner Submenu tab
             ["Adv-Options.html","Recommended options"]
         ]
     ],
-
     //Etiquettes tab
     ["Etiquettes.html","Arcade Etiquettes"],
-
     //AboutAuthors tab
     ["AboutAuthors.html","About Authors"],
-
     //OfficialLinks tab
     ["OfficialLinks.html","Official links"],
-
     //Contacts tab
     ["Contacts.html","Contacts"]
 ];
 
-/* Rendering */
-
-renderMenu(menu.length,menu);
-
-/* helper functions */
-
 /* drop down menu button click */
-function DropdownClick(e,listnum)
+function DropdownClick(e)
 {
-
-    console.log(listnum);
-    var DropDownButton = this.document.getElementById("Button"+listnum);
-    var Submenu = this.document.getElementById("submenu"+listnum);
-    html="";
-
-    //Expand submenu
-    if(DropDownButton.innerHTML=='v'){
-        DropDownButton.innerHTML='^';
-        for(let i=0;i<menu[listnum][2].length;i++){
-            html += '<li><a href='+menu[listnum][2][i][0]+'>'+menu[listnum][2][i][1]+'</a></li>';
-        }
-        Submenu.innerHTML += html;
+    var list = e.nextElementSibling;
+    if (list.style.display == "none"){
+        list.style.display = "inline-block";
+        e.textContent = "^";
     }
-    //Contract menu
-    else{
-        DropDownButton.innerHTML='v';
-        Submenu.innerHTML = "";
+    else {
+        list.style.display = "none";
+        e.textContent = "v";
     }
-
-    html ="";
-
-
 }
 
 function renderMenu(menulength, menulist)
@@ -101,36 +67,43 @@ function renderMenu(menulength, menulist)
 
     //render every array list in menu
     for (let i=0;i<menulength;i++){
-
-        console.log("menulength = " + menulength + " i = "+ i + " menulist[i].length = " + menulist[i].length);
-
-        //adding list item
-        html += '<li '
-
         //checks if the page is a current page
         if(menu[i][0] == currentpage){
-            //add active class
-            html += 'class="active">';
+            html += '<li class="active">';
         }
         else{
-            //do not add active class
-            html += '>';
+            html += '<li>';
         }
 
         //adding href
         html += '<a href='+menu[i][0]+'>'+menu[i][1]+'</a>';
         
-        //add a button if theres a submenu
+        //if there is a submenu
         if(menu[i].length == 3){
-            html += '<button class="dropdown-btn" id=Button'+i+' onclick="DropdownClick(this,'+ i +')">v</button></a> <ul id="submenu'+i+'"></ul>';
+            html += '<button class="dropdown-btn" onclick="DropdownClick(this)">v</button><ul class="submenu" style="display:none">';
+            for (let j=0; j<menu[i][2].length; j++){
+                if(menu[i][2][j][0] == currentpage){
+                    html += '<li class="active">';
+                }
+                else{
+                    html += '<li>';
+                }
+                html += '<a href='+menu[i][2][j][0]+'>'+menu[i][2][j][1]+'</a></li>';
+            }
+            html += '</ul>';
         }
-
         html += '</li>';
-        
-        document.getElementById('main_menu').innerHTML += html;
-        html = ""; 
     }
-
+    return html;
 }
 
+/* Rendering */
 
+document.getElementById('main_menu').innerHTML = renderMenu(menu.length,menu);
+var current = document.getElementById('main_menu').getElementsByClassName('active');
+if(current[0].parentElement.className == 'submenu'){
+    current[0].parentElement.style.display = 'inline-block';
+}
+else if (current[0].querySelector('.submenu')){
+    current[0].querySelector('.submenu').style.display = 'inline-block';
+}
